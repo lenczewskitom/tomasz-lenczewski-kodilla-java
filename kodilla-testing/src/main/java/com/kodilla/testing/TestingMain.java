91af09653973c052de85e20cd3bd981709a0cfa7
lenczewskitom <lenczewskitom@gmail.com>
2022-05-11 15:29:02 +0200

    6.2 Added Calculator class and Calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,20 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator calculatorAdd = new Calculator();
+        Calculator calculatorSubtract = new Calculator();
+
+        Integer addResult = calculatorAdd.add(6,10);
+        Integer subtractResult = calculatorSubtract.subtract(20,5);
+
+        if (addResult.equals(16) && subtractResult.equals(15)) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
