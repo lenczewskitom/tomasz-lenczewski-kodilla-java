78e25872bb0586c537ef281e0216a29b3f75d6e4
lenczewskitom <lenczewskitom@gmail.com>
2022-05-27 13:41:07 +0200

    7.5 Added class ArrayOperations and ArrayOperationsTestSuite

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -0,0 +1,18 @@
+package com.kodilla.stream.array;
+
+import java.util.stream.IntStream;
+
+public interface ArrayOperations {
+    static double getAverage(int[] numbers) {
+        IntStream.range(0, numbers.length)
+                .forEach(n -> System.out.println(numbers[n]));
+
+
+        double averageOfNumbers = IntStream.range(0, numbers.length)
+                .mapToDouble(n -> numbers[n])
+                .average()
+                .getAsDouble();
+
+        return averageOfNumbers;
+    }
+}
