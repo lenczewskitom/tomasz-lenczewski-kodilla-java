81ecd3d94e12ae84ef0a9b04ded74a3a9af55027
lenczewskitom <lenczewskitom@gmail.com>
2022-07-10 20:43:01 +0200

    14.2 Added Library class

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -0,0 +1,41 @@
+package com.kodilla.spring.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LibraryTestSuite {
+
+    @Test
+    void testLoadFromDb() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
+
+        //When
+        library.loadFromDb();
+
+        //Then
+        //do nothing
+    }
+
+    @Test
+    void testSaveToDb() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
+
+        //When
+        library.saveToDb();
+
+        //Then
+        //do nothing
+    }
+}
