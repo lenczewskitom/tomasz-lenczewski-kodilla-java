8f472c17a5bc2fa36c3802fe4135198e1f6a63b5
lenczewskitom <lenczewskitom@gmail.com>
2022-07-25 13:25:30 +0200

    15.6 Added Debugger

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/loop/TestLoopAndConditionalBreakpoints.java
@@ -0,0 +1,20 @@
+package com.kodilla.testing.loop;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestLoopAndConditionalBreakpoints {
+
+    @Test
+    void testLoop() {
+        //Given
+        long sum = 0;
+        //When
+        for (int n = 0; n < 1000; n++) {
+            sum += n;
+            System.out.println("[" + n + "] Sum equals: " + sum);
+        }
+        //Then
+        assertEquals(499500, sum);
+    }
+}
