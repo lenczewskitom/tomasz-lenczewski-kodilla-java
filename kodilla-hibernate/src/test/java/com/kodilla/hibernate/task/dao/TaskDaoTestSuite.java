a3e667bf5ba9ed56abb84917de146e4b64e27416
lenczewskitom <lenczewskitom@gmail.com>
2022-08-01 10:23:42 +0200

    Added 17.2 TaskDao

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
@@ -0,0 +1,55 @@
+package com.kodilla.hibernate.task.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.kodilla.hibernate.task.Task;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootTest
+public class TaskDaoTestSuite {
+
+    private static final String DESCRIPTION = "Test: Learn Hibernate";
+
+    @Autowired
+    private TaskDao taskDao;
+
+    @Test
+    void testTaskDaoSave() {
+        //Given
+        Task task = new Task(DESCRIPTION, 7);
+
+        //When
+        taskDao.save(task);
+
+        //Then
+        int id = task.getId();
+        Optional<Task> readTask = taskDao.findById(id);
+        assertTrue(readTask.isPresent());
+
+        //CleanUp
+        taskDao.deleteAllById(id);
+    }
+
+    @Test
+    void testTaskDaoFindByDuration() {
+        //Given
+        Task task = new Task(DESCRIPTION, 7);
+        taskDao.save(task);
+        int duration = task.getDuration();
+
+        //When
+        List<Task> readTasks = taskDao.findByDuration(duration);
+
+        //Then
+        assertEquals(1, readTasks.size());
+
+        //CleanUp
+        int id = readTasks.get(0).getId();
+        taskDao.deleteById(id);
+    }
+}
