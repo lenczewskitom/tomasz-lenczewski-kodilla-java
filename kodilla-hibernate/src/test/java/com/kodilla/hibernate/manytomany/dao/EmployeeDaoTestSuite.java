dcea73220482436676ed1fdf8e2d71718861fd5f
lenczewskitom <lenczewski.tom@gmail.com>
2022-10-19 15:29:36 +0200

    24.2 Wzorzec Dekorator

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/EmployeeDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/EmployeeDaoTestSuite.java
@@ -1,51 +1,51 @@
-//package com.kodilla.hibernate.manytomany.dao;
-//
-//import com.kodilla.hibernate.manytomany.Company;
-//import com.kodilla.hibernate.manytomany.Employee;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//
-//@SpringBootTest
-//public class EmployeeDaoTestSuite {
-//    @Autowired
-//    EmployeeDao employeeDao;
-//
-//    @Test
-//    public void testNamedQuery() {
-//        //Given
-//        Employee johnSmith = new Employee("John", "Smith");
-//        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
-//        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
-//
-//        Company softwareMachine = new Company("Software Machine");
-//        Company dataMaesters = new Company("Data Maesters");
-//        Company greyMatter = new Company("Grey Matter");
-//
-//        softwareMachine.getEmployees().add(johnSmith);
-//        dataMaesters.getEmployees().add(stephanieClarckson);
-//        dataMaesters.getEmployees().add(lindaKovalsky);
-//        greyMatter.getEmployees().add(johnSmith);
-//        greyMatter.getEmployees().add(lindaKovalsky);
-//
-//        johnSmith.getCompanies().add(softwareMachine);
-//        johnSmith.getCompanies().add(greyMatter);
-//        stephanieClarckson.getCompanies().add(dataMaesters);
-//        lindaKovalsky.getCompanies().add(dataMaesters);
-//        lindaKovalsky.getCompanies().add(greyMatter);
-//
-//        //When
-//        List<Employee> result = employeeDao.retrieveEmployeeByName("Smith");
-//
-//        //Then
-//        assertEquals(1, result.size());
-//
-//        //Cleanup
-//        employeeDao.deleteAll();
-//    }
-//}
+package com.kodilla.hibernate.manytomany.dao;
+
+import com.kodilla.hibernate.manytomany.Company;
+import com.kodilla.hibernate.manytomany.Employee;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+public class EmployeeDaoTestSuite {
+    @Autowired
+    EmployeeDao employeeDao;
+
+    @Test
+    public void testNamedQuery() {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        dataMaesters.getEmployees().add(stephanieClarckson);
+        dataMaesters.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(johnSmith);
+        greyMatter.getEmployees().add(lindaKovalsky);
+
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+
+        //When
+        List<Employee> result = employeeDao.retrieveEmployeeByName("Smith");
+
+        //Then
+        assertEquals(1, result.size());
+
+        //Cleanup
+        employeeDao.deleteAll();
+    }
+}
